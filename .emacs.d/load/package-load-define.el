7a234e0090c1cc04c6971abdffc2d726f922f25d
Jeoygin <jeoygin@gmail.com>
2015-09-13 17:47:11 +0800

    define load-package-xxx functions for all xxx-load.el in load foler

--- .emacs.d/load/package-load-define.el
+++ .emacs.d/load/package-load-define.el
@@ -1,70 +1,12 @@
-(defun load-package-cedet()
-  (interactive)
-  (require 'cedet-load))
+(defmacro create-load-package-defun (name)
+  (list 'defun (intern (concat "load-package-" name)) '()
+    (list 'interactive)
+    (list 'require (list 'quote (intern (concat name "-load"))))))
 
-(defun load-package-jdee()
-  (interactive)
-  (require 'jdee-load))
-
-(defun load-package-yasnippet()
-  (interactive)
-  (require 'yasnippet-load))
-
-(defun load-package-table()
-  (interactive)
-  (require 'table-load))
-
-(defun load-package-jalopy()
-  (interactive)
-  (require 'jalopy-load))
-
-(defun load-package-flymake()
-  (interactive)
-  (require 'flymake-load))
-
-(defun load-package-jmaker()
-  (interactive)
-  (require 'jmaker-load))
-
-(defun load-package-jsee()
-  (interactive)
-  (require 'jsee-load))
-
-(defun load-package-ecb()
-  (interactive)
-  (require 'ecb-load))
-
-(defun load-package-elib()
-  (interactive)
-  (require 'elib-load))
-
-(defun load-package-cscope()
-  (interactive)
-  (require 'cscope-load))
-
-(defun load-package-x-clipboard()
-  (interactive)
-  (require 'x-clipboard-load))
-
-(defun load-package-bracket()
-  (interactive)
-  (require 'bracket-load))
-
-(defun load-package-tab()
-  (interactive)
-  (require 'tab-load))
-
-(defun load-package-color-theme()
-  (interactive)
-  (require 'color-theme-load))
-
-(defun load-package-elpa()
-  (interactive)
-  (require 'elpa-load))
-
-(defun load-package-go()
-  (interactive)
-  (require 'go-load))
+(dolist (file (directory-files (file-name-directory load-file-name) nil "^.+-load\\.el$"))
+  (let ((package-name (replace-regexp-in-string "-load\\.el$" "" file)))
+    (message "%s" package-name)
+    (eval (macroexpand `(create-load-package-defun ,package-name)))))
 
 (defun load-package-java()
   (interactive)
