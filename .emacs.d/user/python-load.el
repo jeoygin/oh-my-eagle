7bc1fcb50ddb053380643e19a8862c2919fcc321
Jeoygin Wang <jeoygin@gmail.com>
2016-06-28 23:22:23 +0800

    Update python-load

--- .emacs.d/user/python-load.el
+++ .emacs.d/user/python-load.el
@@ -1,11 +1,24 @@
 (add-to-list 'load-path (concat (file-name-directory #$) "python"))
+(require 'use-package)
 
-(autoload 'python-mode "python-mode" "Python Mode." t)
-(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
-(add-to-list 'interpreter-mode-alist '("python" . python-mode))
+(use-package jedi
+  :ensure t
+  :defer t
+  :init
+  :config
+  )
 
-(add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:setup-keys t)
-(setq jedi:complete-on-dot t)
+(use-package python-mode
+  :ensure t
+  :defer t
+  :init
+  (autoload 'python-mode "python-mode" "Python Mode." t)
+  (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+  (add-to-list 'interpreter-mode-alist '("python" . python-mode))
+  :config
+  (add-hook 'python-mode-hook 'jedi:setup)
+  (setq jedi:setup-keys t)
+  (setq jedi:complete-on-dot t)
+  )
 
 (provide 'python-load)
