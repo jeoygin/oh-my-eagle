c38bcbe112ce9dd048b1b14b551724012e86bd95
Jeoygin Wang <jeoygin@gmail.com>
2016-07-05 21:23:18 +0800

    Fix problem of loading theme when starting daemon emacs

--- .emacs.d/user/color-theme-load.el
+++ .emacs.d/user/color-theme-load.el
@@ -1,6 +1,17 @@
 (add-to-list 'custom-theme-load-path (concat (file-name-directory #$) "themes/emacs-color-theme-solarized"))
-(load-theme 'solarized t)
-(set-frame-parameter nil 'background-mode 'dark)
-(set-terminal-parameter nil 'background-mode 'dark)
+
+(defun nut/load-solarized-theme ()
+  (interactive)
+  (set-frame-parameter nil 'background-mode 'dark)
+  (set-terminal-parameter nil 'background-mode 'dark)
+  (load-theme 'solarized t))
+
+(defun load-solarized-theme (frame)
+  (select-frame frame)
+  (nut/load-solarized-theme))
+
+(if (daemonp)
+    (add-hook 'after-make-frame-functions #'load-solarized-theme)
+  (nut/load-solarized-theme))
 
 (provide 'color-theme-load)
